7a1e81f94c2f90e7c9350b4a984ec2ae758a29e4
MinThetMaung <minthetmaung96@gmail.com>
2021-01-01 17:17:54 +0630

    fetch image with sdwebimage

--- Podcasts/Models/Podcast.swift
+++ Podcasts/Models/Podcast.swift
@@ -10,4 +10,7 @@ import Foundation
 struct Podcast: Decodable {
     var trackName: String?
     var artistName: String?
+    var trackCount: Int?
+    var artworkUrl600: String?
+    var feedUrl: String?
 }
